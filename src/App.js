7604ce0b743eccc9e81e3c62bb1f9b9d55b3acf2
liuhanru <isuekey@163.com>
2020-09-04 20:05:35 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+import * as routers from './router.js';
+
+function switchRoute(routers=[]) {
+  return routers.map(route => {
+    return (
+      <Route path={route.path}>
+        <route.comp />
+      </Route>
+    );
+  });
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        { switchRoute(routers.routes) }
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
